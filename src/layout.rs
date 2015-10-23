409f880072567bb4f91ac6a9fae5ec3c36a26441
Douglas Campos <qmx@qmx.me>
2015-10-23 21:56:14 -0200

    introduce front matter

--- src/layout.rs
+++ src/layout.rs
@@ -17,8 +17,13 @@ impl Layout {
         let mut content = String::new();
         let mut f = File::open(src).unwrap();
         f.read_to_string(&mut content);
+        let parts = content.split("---\n").collect::<Vec<_>>();
+        if parts.len() != 3 {
+            panic!("front matter is required for layout files");
+        }
+        let (front_matter, template) = (parts[1], parts[2]);
         let fname = src.file_stem().unwrap().to_str().unwrap().to_owned();
-        Layout { name: fname, template: content }
+        Layout { name: fname, template: template.to_owned() }
     }
 
     pub fn render(&self, data: HashMap<String, String>) -> String {
@@ -52,4 +57,10 @@ fn new_layout_from_file() {
     data.insert("content".to_owned(), "liquid".to_owned());
     assert_eq!(layout.render(data), "Hello liquid\n".to_owned());
     assert_eq!(layout.name(), "main");
+}
+
+#[test]
+#[should_panic]
+fn front_matter_is_required() {
+    let layout = Layout::new(Path::new("fixtures/000/_layouts/main.html"));
 }
