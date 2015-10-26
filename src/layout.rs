4782e766792db9a370d01d9114d2513c3fc64970
Douglas Campos <qmx@qmx.me>
2015-10-26 09:27:40 -0200

    introduce the concept of optional layout

--- src/layout.rs
+++ src/layout.rs
@@ -9,7 +9,8 @@ use yaml_rust::{Yaml, YamlLoader};
 #[derive(Debug)]
 pub struct Layout {
     name: String,
-    template: String
+    template: String,
+    layout: Option<String>
 }
 
 impl Layout {
@@ -24,19 +25,21 @@ impl Layout {
         }
         let (front_matter, template) = (parts[1].trim(), parts[2]);
         let fname = src.file_stem().unwrap().to_str().unwrap().to_owned();
-        if !front_matter.is_empty() {
-            let doc = match YamlLoader::load_from_str(&front_matter) {
+        let layout = if !front_matter.is_empty() {
+            match YamlLoader::load_from_str(&front_matter) {
                 Ok(yaml_vec) => {
                     if yaml_vec.len() > 0 {
-                        yaml_vec[0].clone()
+                        Some(yaml_vec[0]["layout"].as_str().unwrap().to_owned())
                     } else {
-                        panic!("boo");
+                        None
                     }
                 },
-                Err(why) => panic!("buh")
-            };
-        }
-        Layout { name: fname, template: template.to_owned() }
+                Err(why) => None
+            }
+        } else {
+            None
+        };
+        Layout { name: fname, template: template.to_owned(), layout:layout }
     }
 
     pub fn render(&self, data: HashMap<String, String>) -> String {
@@ -52,12 +55,16 @@ impl Layout {
     pub fn name(&self) -> String {
         self.name.clone()
     }
+
+    pub fn layout(&self) -> Option<String> {
+        self.layout.clone()
+    }
 }
 
 
 #[test]
 fn renders_the_freaking_layout() {
-    let layout = Layout { name:"".to_owned(), template: "my {{content}}".to_owned() };
+    let layout = Layout { name:"".to_owned(), template: "my {{content}}".to_owned(), layout: None };
     let mut data = HashMap::new();
     data.insert("content".to_owned(), "hello world".to_owned());
     assert_eq!(layout.render(data), "my hello world".to_owned());
@@ -76,4 +83,12 @@ fn new_layout_from_file() {
 #[should_panic]
 fn front_matter_is_required() {
     let layout = Layout::new(Path::new("fixtures/000/_layouts/main.html"));
+}
+
+#[test]
+fn extracts_config_from_front_matter() {
+    let post_layout = Layout::new(Path::new("fixtures/002/_layouts/post.html"));
+    assert_eq!(post_layout.layout(), Some("main".to_owned()));
+    let main_layout = Layout::new(Path::new("fixtures/002/_layouts/main.html"));
+    assert_eq!(main_layout.layout(), None);
 }
