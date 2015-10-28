864d11619485cc6b65ca30c285f203eddfa3c0d5
Douglas Campos <qmx@qmx.me>
2015-10-28 10:21:40 -0200

    parse most stuff from filename

--- src/post.rs
+++ src/post.rs
@@ -17,13 +17,16 @@ impl Post {
         let filename = src.file_stem().unwrap().to_str().unwrap();
         let re = Regex::new(r"^(\d{4}-\d{2}-\d{2})-(\d{3})-(.+)$").unwrap();
         let cap = re.captures(filename).unwrap();
+        let date_str = cap.at(1).unwrap();
+        let seq = cap.at(2).unwrap();
+        let slug = cap.at(3).unwrap();
         Post {
             title: "".to_owned(),
-            slug: cap.at(3).unwrap().to_owned(),
+            slug: slug.to_owned(),
             content: "".to_owned(),
             layout: Some("".to_owned()),
-            date: "".to_owned(),
-            seq: 0u8
+            date: date_str.to_owned(),
+            seq: seq.parse::<u8>().unwrap()
         }
     }
 
@@ -33,7 +36,9 @@ impl Post {
 }
 
 #[test]
-fn constructs_slug_from_filename() {
+fn constructs_post_from_filename() {
     let post = Post::new(Path::new("fixtures/003/_posts/2015-10-26-001-merry-xmas.markdown"));
-    assert_eq!(post.slug(), "merry-xmas")
+    assert_eq!(post.slug(), "merry-xmas");
+    assert_eq!(post.date, "2015-10-26".to_owned());
+    assert_eq!(post.seq, 1);
 }
