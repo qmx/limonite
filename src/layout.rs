565ea93923047e4ca4f73a912efc853c7f9cbacf
Douglas Campos <qmx@qmx.me>
2015-11-10 23:49:45 -0200

    renders layouts recursively

--- src/layout.rs
+++ src/layout.rs
@@ -1,8 +1,10 @@
 use std::default::Default;
 use std::path::Path;
 use std::collections::HashMap;
+use std::fs;
 use liquid::{self, Renderable, LiquidOptions, Context};
 use util;
+
 
 #[derive(Debug)]
 pub struct Layout {
@@ -10,7 +12,6 @@ pub struct Layout {
     template: String,
     layout: Option<String>
 }
-
 
 impl Layout {
 
@@ -72,4 +73,46 @@ fn extracts_config_from_front_matter() {
     assert_eq!(post_layout.layout(), Some("main".to_owned()));
     let main_layout = Layout::new(Path::new("fixtures/002/_layouts/main.html"));
     assert_eq!(main_layout.layout(), None);
+}
+
+pub struct LayoutStore {
+    layouts: HashMap<String, Layout>
+}
+
+impl<'a> LayoutStore {
+    pub fn new(layout_dir: &Path) -> LayoutStore {
+        let mut layouts: HashMap<String, Layout> = HashMap::new();
+        for entry in fs::read_dir(&layout_dir).unwrap() {
+            let layout_path = entry.unwrap().path();
+            let fname = layout_path.file_name().unwrap().to_str().unwrap();
+            if !fname.starts_with(".") && fname.ends_with("html") {
+                let layout = Layout::new(&layout_path);
+                layouts.insert(layout.name(), layout);
+            }
+        }
+        LayoutStore {
+            layouts: layouts
+        }
+    }
+
+    pub fn render(&self, layout_name: &'a str, content: String, context: HashMap<String, String>) -> String {
+        let mut buffer = String::new();
+        let layout = self.layouts.get(layout_name).unwrap();
+        let data = HashMap::new();
+        buffer = layout.render(content, data);
+        match layout.layout() {
+            Some(l) => {
+                buffer = self.render(&l, buffer, HashMap::new());
+            },
+            None => {}
+        }
+        buffer
+    }
+}
+
+#[test]
+fn renders_recursive_layout() {
+    let layout_store = LayoutStore::new(Path::new("fixtures/004/_layouts"));
+    let post_content = layout_store.render("post", "Post Body".to_owned(), HashMap::new());
+    assert_eq!(post_content, "Main\n\nPost Body\n".to_owned());
 }
