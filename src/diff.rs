f53fffa1b0e2f24b13be0dcadff6693e87939b47
Douglas Campos <qmx@qmx.me>
2015-12-02 09:20:46 -0200

    better naming for diff

--- src/diff.rs
+++ src/diff.rs
@@ -4,11 +4,11 @@ use crypto::digest::Digest;
 use std::fs::File;
 use std::io::prelude::*;
 
-pub fn diff(f1_path: &str, f2_path: &str) -> bool {
-    diff_path(Path::new(f1_path), Path::new(f2_path))
+pub fn compare(f1_path: &str, f2_path: &str) -> bool {
+    compare_paths(Path::new(f1_path), Path::new(f2_path))
 }
 
-pub fn diff_path(p1: &Path, p2: &Path) -> bool {
+pub fn compare_paths(p1: &Path, p2: &Path) -> bool {
     let mut f1 = match File::open(&p1) {
         Ok(f) => f,
         Err(_) => {
@@ -32,14 +32,19 @@ pub fn diff_path(p1: &Path, p2: &Path) -> bool {
     let mut h2 = Sha1::new();
     h2.input(&b2);
     let r2 = h1.result_str();
-    return r1 != r2;
+    return r1 == r2;
 }
 
 
 #[test]
+fn compares_file_contents_happy() {
+    assert!(compare("fixtures/008/a", "fixtures/008/b"));
+}
+
+#[test]
 #[should_panic]
 fn compares_file_contents() {
-    assert!(diff("fixtures/008/a", "fixtures/008/b"));
-    assert!(!diff("fixtures/008/a", "fixtures/008/a"));
-    assert!(!diff("fixtures/008/b", "fixtures/008/b"));
+    assert!(compare("fixtures/008/a", "fixtures/008/b"));
+    assert!(!compare("fixtures/008/a", "fixtures/008/a"));
+    assert!(!compare("fixtures/008/b", "fixtures/008/b"));
 }
