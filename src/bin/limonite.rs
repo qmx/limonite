cef0771bc80b7267ce488168b0e809d88d4303c2
Douglas Campos <qmx@qmx.me>
2016-01-14 20:16:11 -0200

    add a better cli interface

--- src/bin/limonite.rs
+++ src/bin/limonite.rs
@@ -1,16 +1,23 @@
 extern crate limonite;
+extern crate clap;
+use clap::{Arg, App};
 
 use limonite::site::Site;
+use limonite::GIT_VERSION;
 use std::env;
 use std::path::Path;
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    if args.len() != 3 {
-        panic!("wrong argument count");
-    }
-    let src_path = Path::new(&args[1]);
-    let dst_path = Path::new(&args[2]);
+    let matches = App::new("limonite")
+        .author("Douglas Campos <qmx@qmx.me>")
+        .version(GIT_VERSION)
+        .about("blazingly-fast static site and blog generator")
+        .args_from_usage(
+            "<SOURCE_PATH> 'path to folder containing the site structure'
+            <TARGET_PATH> 'path to where limonite should write the generated files'")
+        .get_matches();
+    let src_path = Path::new(matches.value_of("SOURCE_PATH").unwrap());
+    let dst_path = Path::new(matches.value_of("TARGET_PATH").unwrap());
     let site = Site::new(&src_path);
     site.generate(&dst_path);
 }
